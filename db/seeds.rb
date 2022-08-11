ed44fefde57a94b4cb7eadad896da9de4f132ee0
Victor Prete <vprete@tuta.io>
2022-08-11 12:06:33 -0400

    Add db seed with tmdb api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts 'Deleting movies'
+Movie.destroy_all
+
+puts 'Creating movies from Movies API...'
+
+movies['results'].each do |movie|
+  new_movie = Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original/#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+  puts "Movie #{new_movie.title} was created"
+end
+
+puts 'All done!'
