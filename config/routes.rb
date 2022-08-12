8de91340e75fd2cefae82b7b694e023a5aca3389
Victor Prete <vprete@tuta.io>
2022-08-12 12:01:34 -0400

    Deleted /lists path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
 
-  resources :lists, only: %i[index show new create destroy] do
+  resources :lists, only: %i[show new create destroy] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
