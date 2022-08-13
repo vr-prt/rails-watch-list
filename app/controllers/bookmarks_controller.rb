7bc8d54f3c4887ea72559dbb8fd0d0486b5ff41b
Victor Prete <vprete@tuta.io>
2022-08-13 14:49:05 -0400

    Changed baseline

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,7 +19,8 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id]).destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
     redirect_to list_path(@bookmark.list)
   end
 
---
+++ app/javascript/controllers/banner_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="banner"
+export default class extends Controller {
+  connect() {
+    console.log("test")
+  }
+}
